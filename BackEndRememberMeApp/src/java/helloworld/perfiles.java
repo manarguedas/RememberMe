971ac25e2a4ac0637c3bf07e41584ba20f11c562
Zecklet <xecklet@gmail.com>
2014-08-29 16:32:45 -0600

    Servlet
    Agregue un servlet al servido capaz de leer y enviar un json para la
    comunicación

---
+++ BackEndRememberMeApp/src/java/helloworld/perfiles.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloworld;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author MarcoNey
+ */
+public class perfiles extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("application/json");
+        AddEncabezados(response);
+        PrintWriter out = response.getWriter();
+        try {
+            String json = request.getParameter("test");
+            /* TODO output your page here. You may use following sample code. */
+            out.println(json);
+            out.flush();
+        } finally {
+            out.close();
+        }
+    }
+
+    private String GetJSON(HttpServletRequest request) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } finally {
+            reader.close();
+        }
+        return sb.toString();
+    }
+    
+      private void AddEncabezados(HttpServletResponse response){
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+        response.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
+        response.addHeader("Access-Control-Max-Age", "1728000");
+    }
+    
+      protected void enviarJson(HttpServletRequest request, HttpServletResponse response){
+        try {
+        //TODO: externalize the Allow-Origin
+            AddEncabezados(response);
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.println("{");
+        out.println("\"First Name\": \"Devesh\",");
+        out.println("\"Last Name\": \"Sharma\"");
+        out.println("}");
+        out.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    }
+      
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
