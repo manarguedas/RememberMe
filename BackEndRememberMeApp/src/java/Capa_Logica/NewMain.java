f5cf59de62766f131932015f14d635f80256ef1b
Zecklet <xecklet@gmail.com>
2014-08-30 19:37:48 -0600

    Arreglo
    Arreglo problemas de compilación que se le presentaron a gustavo

--- BackEndRememberMeApp/src/java/Capa_Logica/NewMain.java
+++ BackEndRememberMeApp/src/java/Capa_Logica/NewMain.java
@@ -6,76 +6,11 @@
 
 package Capa_Logica;
 
-import Capa_Datos.AgregarPerfil_Datos;
-import Capa_Datos.Conexion;
-import Capa_Datos.GestionarEventos_Datos;
-import Capa_Datos.ObtenerPerfiles_Datos;
-import Capa_Datos.PrepararQuerrys;
-import Capa_Presentacion.AgregarPerfil_Presentacion;
-import Capa_Presentacion.ConsultarPerfil_Presentacion;
-import com.sun.imageio.plugins.jpeg.JPEG;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  *
  * @author gustavovargas
  */
 public class NewMain {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws SQLException {
-        
-        //String reader = "{\"per\":{\"nom\":\"Ney\",\"ape\":\"Rojas\",\"nac\":\"1820-01-04\",\"def\":\"1980-01-08\",\"url\":\"www.remembermeapp.com/recursos/fotos/121.png\",\"id\":\"121\"}}";
-        Conexion c = new Conexion();
-        c.crearConexion();
-//        c.ejecutarSQL("INSERT INTO ADMINISTRADORES  ( nombre, apellidos, fecha_nacimiento, fecha_defuncion, dir_foto) VALUES('Ney','Rojas','Tue Jan 04 00:00:00 CST 1820','Tue Jan 08 00:00:00 CST 1980','www.remembermeapp.com/recursos/fotos/121.png')");
-        
-        
-//        
-        String reader = "{\"idd\":\"2\", \"eve\":[{\"id\":\"32\",\"nom\":\"Vela\",\"des\":\"En la iglesia de no se donde se realizará la vela de juan perez\",\"lug\":\"Alajuela\",\"fec\":\"2014-23-12\",\"hor\":\"11:00\"},{\"id\":\"64\",\"nom\":\"Vela\",\"des\":\"En la iglesia de no se donde se realizará la vela de juan perez\",\"lug\":\"Alajuela\",\"fec\":\"2014-23-12\",\"hor\":\"11:00\"}]}";
-        ParseJson_Evento p = new ParseJson_Evento();
-//        Evento i = p.ParseEventoModelo(reader);
-//        GestionarEventos_Datos r = new GestionarEventos_Datos();
-//        r.AgregarEvento(i);
-//        
-        
-//        
-//        PrepararQuerrys h = new PrepararQuerrys();
-//        System.out.println(h.AgregarEvento(i));
-//        List<Evento> eventos = new ArrayList();
-//        for (int i = 0; i < 10; i++) {
-//            eventos.add(p.ParseEventoModelo(reader));
-//            System.out.println("id evento: "+eventos.get(i).getDescripcion());
-//        }
-//        System.out.println(p.ParsePerfilJson(eventos));
-//        
-//        
-        //System.out.println(eventos[0].getId());
-        //System.out.println(eventos.length);
-        //System.out.println(eventos[0].getIdDifunto());
-        //System.out.println(eventos[5].getIdDifunto());
-//        System.out.println(p.ParsePerfilJson(eventos));
-        //ResultSet p = c.ejecutarSQLSelect("select * from ADMINISTRADORES");
-        //p.
-//        AgregarPerfil_Presentacion e = new AgregarPerfil_Presentacion();
-//        e.AgregarPerfil(reader);
-        //String u = "INSERT INTO ADMINISTRADORES  (pk_administradores , Correo ) VALUES (2, 'nose.com')";
-        //c.ejecutarSQL(u);
-        //ParseJson_Perfil py = new ParseJson_Perfil();
-        //ObtenerPerfiles_Datos r = new ObtenerPerfiles_Datos();
-        //Perfil l = r.ObtenerPerfilDifunto(1);
-        //AgregarPerfil_Datos p = new AgregarPerfil_Datos();
-       // p.AgregarPerfil(l);
-        //System.out.println(p.ParsePerfilJson(l));
-        //p.ParsePerfilModelo(reader);
-        // TODO code application logic here
-        //ConsultarPerfil_Presentacion p = new ConsultarPerfil_Presentacion();
-        ///System.out.println(p.ConsultarPerfil(030303));
-    }
     
 }
--- BackEndRememberMeApp/src/java/Capa_Presentacion/ConsultarEventos_Presentacion.java
+++ BackEndRememberMeApp/src/java/Capa_Presentacion/ConsultarEventos_Presentacion.java
@@ -7,6 +7,7 @@
 package Capa_Presentacion;
 
 import Capa_Logica.ConsultarEventos_Logica;
+import java.sql.SQLException;
 
 /**
  *
@@ -14,7 +15,7 @@ import Capa_Logica.ConsultarEventos_Logica;
  */
 public class ConsultarEventos_Presentacion {
     
-    public String ConsultarEventos(int idDifunto){
+    public String ConsultarEventos(int idDifunto) throws SQLException{
         ConsultarEventos_Logica consultar = new ConsultarEventos_Logica();
         return consultar.ConsultarEventos(idDifunto);
     }
