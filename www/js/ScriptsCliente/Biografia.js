4ac5883f5cf5e75b7b379c1149472f2e2777914a
Zecklet <xecklet@gmail.com>
2014-09-15 13:10:36 -0600

    Funcionalidad de localización de la tumba realizada por Manuel

--- www/js/ScriptsCliente/Biografia.js
+++ www/js/ScriptsCliente/Biografia.js
@@ -3,32 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
- 
-function Biografia(){
+
+var CascaronBiografia = '<div class="panel panel-primary" id="{id}"> \
+                        <div class="panel-heading"> \
+                            <h5 class="panel-title">{tit}</h5>\
+                        </div>\
+                        <div class="panel-body">\
+                            <small>{des}</small>\
+                        </div>\
+                    </div>';
+
+function Biografia() {
     this.id = "";
     this.nom = "";
     this.des = "";
 }
 
-function AdministradorBiografia(){
+function AdministradorBiografia() {
     this.idd = "";
     this.bio = new Array();
-    
-    
-    this.AgregarBiografia = function AgregarBiografia(pTitulo,pDescripcion){
+
+
+    this.AgregarBiografia = function AgregarBiografia(pTitulo, pDescripcion) {
         var mBio = new Biografia();
         mBio.des = pDescripcion;
         mBio.nom = pTitulo;
         this.bio[this.bio.length] = mBio;
         //this.bio.ap;
     };
-    
-    this.CargarObjeto = function  CargarJSON(pObjeto){
+
+    this.CargarObjeto = function  CargarJSON(pObjeto) {
         this.bio = pObjeto.bio;
         this.idd = pObjeto.idd;
     };
-    
-    this.EnviarBiografiaCrear = function EnviarBiografiaCrear(pIdDifunto){
+
+    this.EnviarBiografiaCrear = function EnviarBiografiaCrear(pIdDifunto) {
         this.idd = pIdDifunto;
         $.ajax({
             type: kConstantes.post, // it's easier to read GET request parameters
@@ -45,24 +54,48 @@ function AdministradorBiografia(){
             }
         });
     };
-    
-    this.CargarBiografias = function CargarBiografias(pIdDifunto){
+
+    this.CargarBiografias = function CargarBiografias() {
+        var pIdDifunto = 8;//sessionStorage.getItem("idDifunto");
+        alert(pIdDifunto);
         $.ajax({
-            type: kConstantes.get, // it's easier to read GET request parameters
-            url: kConstantes.Servidor + kConstantes.DirBiografias + "?idDifunto="+pIdDifunto,
+            type: kConstantes.get,
+            url: kConstantes.Servidor + kConstantes.DirBiografias + "?idDifunto=" + pIdDifunto,
             dataType: 'JSON',
             data: {
                 json: ""
             },
             success: function(data) {
-                AdminBio.CargarObjeto(data);
+               // AdminBio.CargarObjeto(data);
+                CargarBiografiasHtml(data);
             },
             error: function(data) {
                 alert('No se pudo conectar con el servidor.');
             }
         });
     };
-    
+
 }
 
 var AdminBio = new AdministradorBiografia();
+
+
+function CargarBiografiasHtml(pOb) {
+    alert("entrado");
+    if (pOb === null && pOb.bio === null) {
+        console.log("No hay biografías");
+        return;
+    }
+    var i = 0;
+    var mHtmlResultado= "";
+    var bio;
+    alert("cargando los datos");
+    for (i; i < pOb.bio.length; i++) {
+        bio = CascaronBiografia;
+        bio = replace.replace("{tit}",pOb.bio[i].nom);
+        bio = replace.replace("{des}",pOb.bio[i].des);
+        bio = replace.replace("{id}",pOb.bio[i].id);
+        mHtmlResultado += bio;
+    }
+    $("#biografias").html = mHtmlResultado;
+}
