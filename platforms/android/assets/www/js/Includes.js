faa9bd48d92530ddfd6a53f9216bfe5e5211d655
manu <manu@manu-eMachines-E525>
2014-09-27 19:33:19 -0600

    Agrego manual de usuario

---
+++ platforms/android/assets/www/js/Includes.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.write('<script type="text/javascript" src="cordova.js"></script>');
+document.write('<script type="text/javascript" src="js/vendor/jquery-1.11.0.js"></script>');
+document.write('<script type="text/javascript" src="js/main.js"></script>');
+document.write('<script type="text/javascript" src="js/Barras/NavBars.js"></script>');
+document.write('<script type="text/javascript" src="js/ScriptsCliente/Constantes.js"></script>');
+document.write('<script type="text/javascript" src="js/ScriptsCliente/Ajax.js"></script>');
+document.write('<script type="text/javascript" src="js/ScriptsCliente/PerfilJSON.js"></script>');
+document.write('<script type="text/javascript" src="js/ScriptsCliente/Biografia.js"></script>');
+document.write('<script type="text/javascript" src="js/ScriptsCliente/Comentarios.js"></script>');
+document.write('<script type="text/javascript" src="js/ScriptsCliente/Busquedas.js"></script>');
+document.write('<script type="text/javascript" src="js/ScriptsCliente/Eventos.js"></script>');
+document.write('<script type="text/javascript" src="js/Fotografia.js"></script>');
+document.write('<script type="text/javascript" src="js/LocationGoogleMaps.js"></script>');
+document.write('<script type="text/javascript" src="js/Eventos.js"></script>');
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
